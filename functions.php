4af00b5ab23108e9e04ba363c38776acb59d03e3
Céline V <celine.verreydt.pro@gmail.com>
2023-03-13 06:47:16 +0100

    up

--- functions.php
+++ functions.php
@@ -4,12 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP exercises</title>
 </head>
 <body>
     
 
 <?php
+
+/*Function calls during "Runtime" :
+function say_hello($firstname){
+	echo "<p>Hello $firstname!</p>";
+	echo '<hr>';
+}
+say_hello("Maurice")
+say_hello("Alice");
+say_hello("Jésus");
+say_hello("Judas");
+Just for the fun of it, we can make that code even more elegant by using an array, a loop and the function.
+Do you see how ?
+*/
+
+$nameList = array("Maurice", "Alice", "Jésus", "Judas");
+foreach ($nameList as $name) {
+    echo "<p>Hello $name!</p>";
+	echo '<hr>';
+    };
+
+
+
+//The words need to stay the same. The letters inside the words are shuffled.
 
 $text = array("According", "to", "a", "researcher", "at", "Cambridge", ",", "University", 
 "it", "doesn't", "matter", "in", "what", "order", "the", "letters", "in", "a", "word", "are", ",",
@@ -19,11 +42,20 @@ $text = array("According", "to", "a", "researcher", "at", "Cambridge", ",", "Uni
 "because", "the", "human", "mind", "does", "not", "read", "every", "letter", "by", "itself", 
 "but", "the", "word", "as", "a", "whole", "."); //inefficient, explode() exists
 
+/*should be:
+$text = "According to a researcher at Cambridge University,
+it doesn't matter in what order the letters in a word are,
+the only important thing is that the first and last letter be at the right place.
+The rest can be a total mess and you can still read it without problem.
+This is because the human mind does not read every letter by itself but the word as a whole."
+-> foreach (explode(" ", $text) as $word)
+*/
+
 foreach ($text as $word) {
         echo str_shuffle($word) . " ";
     };
 echo "<br><br>";
-//doesn't keep the first and last letters in place, but that wasn't actually specified, so...?
+
 
 
 //Use a function that capitalizes the first letter of the provided argument.
@@ -43,6 +75,7 @@ $currentYear = date("Y");
 $theDate = date("d/m/Y, H:i:s");
 echo $theDate;
 echo "<br><br>";
+
 
 
 //Create a "Sum" function that takes 2 numbers and returns their sum.
@@ -66,9 +99,11 @@ echo numSum("x", 2);
 echo "<br><br>";
 
 
+
 /*Create a function that takes as argument a string of characters
 and returns an acronym made of the initials of each word.
 Example: "In code we trust!" should return: ICWT).*/
+
 $str = "In code we trust!";
 function acronym($string) {
     $stringArray = explode(" ", $string);
@@ -78,9 +113,12 @@ function acronym($string) {
 };
 acronym($str);
 echo "<br>";
-$suMoPostGame = "Police Locale Opérations Ultra chimères";
+
+//just for kicks
+$suMoPostGame = "Pôle Liéaux Opérations Ultra chimères";
 acronym($suMoPostGame);
 echo "<br><br>";
+
 
 
 /*Create a function that replaces the letters "a" and "e" with "æ".
@@ -109,6 +147,7 @@ foreach ($moreScienceWords as $oneMoreScienceWord) {
 echo "<br><br>";
 
 
+
 /*Create a function to return "notice", "warning" and "error" messages to a user, which takes 2 arguments : 
 the "message", and the "css class" (values: "info", "error", "warning").
 This function would allow us to write this :
@@ -122,7 +161,10 @@ function feedback($message, $class){
     echo '<div class="'.$class.'">'.$classInText.': '.$message.'.</div>';
 };
 feedback("Incorrect email address", "error"); 
-//doesn't address the values thing, but if the goal is to just write in the value name anyway, what are they for...? 
+//doesn't address the values thing
+//(but if the goal is to just write in the value name anyway, what are they for...?)
+
+echo "<br><br>";
 
 
 
@@ -130,11 +172,18 @@ feedback("Incorrect email address", "error");
 one which length is between 1 to 5 letters, the other between 7 and 15 letters.
 The screen will display a title "Generate a new word", and then the two generated words,
 and underneath, a bouton with the text "Generate").*/
+echo "Generate a new word";
+echo "<br>";
+//echo word 1 & 2
+echo "<input type='submit' value='Generate'>";
+
+echo "<br><br>";
+
 
 
 //De-capitalize the string : "STOP YELLING I CAN'T HEAR MYSELF THINKING!!"
 $capitalString = "STOP YELLING I CAN'T HEAR MYSELF THINKING!!";
-ucfirst(strtolower($capitalString));
+echo ucfirst(strtolower($capitalString));
 
 
 /*In your new job, you have to maintain the code of a former developer.
@@ -145,7 +194,12 @@ echo 'The volume of a cone which ray is 5 and height is 2 = ' . $volume . ' cm<s
 // Volume of a cone which ray is 3 and height is 4  
 $volume = 3 * 3 * 3.14 * 4 * (1/3);  
 echo 'The volume of a cone which ray is 3 and height is 4 = ' . $volume . ' cm<sup>3</sup><br />'; */
- 
+function volume($ray, $height){
+    $volume = $ray * $ray * 3.14 * $height * (1/3);
+    echo 'The volume of a cone which ray is' . $ray . 'and height is' . $height . '= ' . $volume . ' cm<sup>3</sup><br />';
+};
+
+
 ?>
 
 </body>
